8a52ae2c59f62473c60f7c2ac85da443a33007c4
James Dunn <jwdunn1@gmail.com>
2018-11-17 22:42:28 -0800

    Update navigation

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-  function toggle() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") 
-      x.className += " responsive";
-    else 
-      x.className = "topnav";
-  }
-  function toggleOff() {
-    var x = document.getElementById("myTopnav");
-      x.className = "topnav";
-  }
+var _info_ = {
+  Version   : "1.0.0",
+  Author    : "Alyce Hoggan and James Dunn",
+  Copyright : "Motivara Corporation",
+  Source    : "github.com/RNSchultz/RNSchultz.github.io"
+};
+
+function toggle() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") 
+    x.className += " responsive";
+  else 
+    x.className = "topnav";
+}
+function toggleOff() {
+  var x = document.getElementById("myTopnav");
+    x.className = "topnav";
+}
+
+// Accordion
+var acc = document.getElementsByClassName("accordion");
+for (var i = 0; i < acc.length; i++)
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("activer");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block")
+      panel.style.display = "none";
+    else
+      panel.style.display = "block";
+    });
